259d710744182682d3824328fd4ed0c726788280
Ebuka <ebukaopara180@gmail.com>
2025-05-02 15:11:06 +0100

    2nd commit

--- bankCodes.py
+++ bankCodes.py
@@ -1,10 +1,12 @@
 import math
+cb = 0
 print("welcome to the online banking application")
 
 def signin():
     global name # username
     global pin # password
     global cb # cureent balance
+    cb = 0
     name = str(input("please create your username: "))
     pin = str(input("please create your 6 digit pin: "))
     if len(pin) == 6:
@@ -45,23 +47,22 @@ def depositinterest(p,r,t):
     return a
 
 def login():
+    global name, pin, cb
     # name1 represent username
     # pin1 represent password
     name1 = str(input("please enter your username: "))
     pin1 = str(input("please enter your pin: "))
     if name1 == name and pin1 == pin:
         print("welcome to the online banking application"+ " " + name)
-        print("please choose the menu down here")
+        print("please choose the menu d own here")
         listmenu = ["1-Deposit","2-Withdraw","3-Tranfer","4-Check Balance","5-Deposit interest rate","6-Calculate comnpound interest", "7-Exist"]
         for b in listmenu:
             print(b)
         choose = int(input("please the number of your choice: "))
-        d = 0 # represent deposit
-        w = 0 # represent withdraw
-        cb = 0 # represent current balance
+        
         if choose == 1:
             d = int(input("Enter the amount of your deposit: "))
-            cb = d
+            cb += d
             print("Your current balance is"+" "+ str(cb))
         elif choose == 2:
             w = int(input("Enter the amount of money you want to enter: "))
@@ -69,59 +70,59 @@ def login():
                 print("your current balance is not sufficient for this transaction")
                 login()
             else:
-                cb = d-w
-            print(str(w)+" "+ "have been withdrawn from your account and your current balance is "+" "+ str(cb))
+                cb -= w
+            print(f"{w} has been withdrawn from your account, your current balance {cb}")
         elif choose == 3:
-            dest = str(input("please enter the acconut number of your destination in 8 digits: "))
-            if len(dest) == 8:
+            dest = input("please enter the acconut number of your destination in 8 digits: ")
+            if len(dest) == 8 and dest.isdigit():
                 amount = int(input("please enter the amount of money you want to tranfer: "))
                 if amount > cb:
                     print("your current balance is not sufficient for this transaction")
                     login()
                 else:
-                    cb = d - amount
-                    print("The transaction of" + " " + str(amount) + " " + "has been transfer to" + "" + str(dest) + " " + "your current balance is" + " " + str(cb))
+                    cb -= amount
+                    print(f"{amount} was transfered to {dest}, Your current balance is {cb}")
             else:
                 print("The transaction has been rejected since the account is invalid")
                 login()
         elif choose == 4:
-            print("Your account balance is"+ " "+ str(cb))
+            print("Your account balance is"+ str(cb))
         elif choose == 5:
-            if d > 50000:
+            if cb > 50000:
                 rate = 3
-            elif d > 30000:
+            elif cb > 30000:
                 rate = 2
             else:
                 rate = 1.5
-                print("Your deposit interest rate"+ " "+ str(rate) + " " + "%")
+                print(f"Your deposit interest rate"+ {rate} + "%")
         elif choose == 6:
-            listOption = ["1-calculate your deposit compound interest based on your CB", "2-Calculate your deposit compound interest based on your deposit input"]
+            listOption = ["1-calculate your deposit compound interest based on your Current balance", "2-Calculate your deposit compound interest based on your deposit input"]
             for n in listOption:
                 print(n)
             choose = int(input("please enter your choice from the option above: "))
             if choose == 1:
-                timing = str(input("How many year do ypu want to invest your money?: "))
-                if d > 50000:
+                timing = float(input("How many year do you want to invest your money?: "))
+                if cb > 50000:
                     ratex = 3/100
-                if d > 30000:
+                elif cb > 30000:
+                    ratex = 2/100
+                else:
+                    ratex = 1.5/100
+                print(f"Your current balance in {timing} years will be: ")
+                print(depositinterest(cb,ratex,timing))
+            elif choose == 2:
+                timming1 = float(input("How many year do you want to invest your money?: "))
+                money = float(input("Please enter the amount of money you would like to deposit: "))
+                if money > 50000:
+                    ratex = 3/100
+                if money > 30000:
                     ratex = 2/100
                 else:
                     ratex = 1.5/100
                 print("Your current balance in"+ " " + "timming"+ " " + "year will be")
-                print(depositinterest(cb,ratex,timing))
-            elif choose == 2:
-                timming1 = str(input("How many year do you want to invest your money?: "))
-                money = str(input("Please enter the amount of money you would like to deposit: "))
-                if d > 50000:
-                    ratex = 3/100
-                if d > 30000:
-                    ratex = 2/100
-                else:
-                    ratex = 1.5/10
-                print("Your current balance in"+ " " + "timming"+ " " + "year will be")
                 print(depositinterest(money,ratex,timming1))
         elif choose == 7:
-            login()
+            exist()
         else:
             print("Option is not available")
             login()
@@ -163,11 +164,11 @@ def mainmenu():
     exist()
 
 def exist():
-    answer = str(input("do you still want to conduct transaction?, Yes or No"))
+    answer = str(input("do you still want to conduct transaction?, Yes or No: " )).lower()
     if answer == "yes":
         login()
-    elif answer == "No":
-        print("Thank you for this app")
+    elif answer == "no":
+        print("Thank you for using this app")
 
     else:
         print("Option not available")
